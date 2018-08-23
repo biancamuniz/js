f20ae9bee24a4228cfbf34bb3068625fec158b16
biancamuniz <biancarolmuniz@gmail.com>
2018-08-23 14:28:02 -0300

    Add files via upload

---
+++ exe1.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+
+    <script type="text/javascript">
+
+        var ingresso, proposta, diferenca;
+
+        ingresso = 170;
+        proposta = prompt("Insira o valor da proposta");
+        diferenca = ingresso - proposta;
+
+        alert("A diferença de valor é de R$ " +diferenca);
+
+    </script>
+
+</head>
+<body>
+
+</body>
+</html>
---
+++ exe2.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+
+    <script type="text/javascript">
+
+        var links, curtidasPubli, totalCurtidas, dias, total;
+
+        links = 6;
+        curtidasPubli = 8;
+        totalCurtidas = links * curtidasPubli;
+        dias = prompt("Informe a quantidade de dias");
+
+        total = dias * totalCurtidas;
+
+        alert("Você recebeu " + total + " curtidas durante " + dias + " dias" );
+
+
+    </script>
+
+</head>
+<body>
+
+</body>
+</html>
